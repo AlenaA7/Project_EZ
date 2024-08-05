9eb32e7fe377d051922fe1ac487366a8a3881218
Alena <shevelevaaly@gmail.com>
2024-08-05 09:50:29 +0300

    all but music

--- scripts/the_end.gd
+++ scripts/the_end.gd
@@ -1,8 +1,11 @@
 extends Area2D
+@onready var sprite = $AnimatedSprite2D
 
 func _on_body_entered(body):
 	if body is Player:
 		body._on_the_end()
+		sprite.play('happy')
+		
 		
  # Replace with function body.
 #Конец игрф
